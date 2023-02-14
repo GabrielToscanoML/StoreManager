796be0c4d81abfd5b1519bbe90f33a73fc989251
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-02-14 15:46:53 -0300

    Atingindo 100% de cobertura de testes da pasta models

--- tests/unit/models/mocks/products.model.mock.js
+++ tests/unit/models/mocks/products.model.mock.js
@@ -13,6 +13,27 @@ const products = [
   }
 ];
 
+const insertProductMock = [
+  {
+    "id": 1,
+    "name": "Martelo de Thor"
+  },
+  {
+    "id": 2,
+    "name": "Traje de encolhimento"
+  },
+  {
+    "id": 3,
+    "name": "Escudo do Capitão América"
+  },
+  {
+    "id": 4,
+    "name": "ProdutoX"
+  }
+];
+
+
 module.exports = {
   products,
+  insertProductMock,
 };
--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -4,7 +4,7 @@ const sinon = require('sinon');
 const { productsModel } = require('../../../src/models');
 
 const connection = require('../../../src/models/connection');
-const { products } = require('./mocks/products.model.mock');
+const { products, insertProductMock } = require('./mocks/products.model.mock');
 
 describe('Testes de unidade do model de produtos', function () {
   it('Recuperando a lista de produtos', async function () {
@@ -14,6 +14,25 @@ describe('Testes de unidade do model de produtos', function () {
   const result = await productsModel.getAllProducts();
   // Assert
   expect(result).to.be.deep.equal(products);
+  });
+
+  it('Recuperando um produto a partir do seu id', async function () {
+    // Arrange
+    sinon.stub(connection, 'execute').resolves([[products[0]]]);
+    // Act
+    const result = await productsModel.getProductById(1);
+    // Assert
+    expect(result).to.be.deep.equal(products[0]);
+  });
+
+  it('Inserindo um produto', async function () {
+    // Arrange
+    sinon.stub(connection, 'execute').resolves([insertProductMock]);
+    // Act
+    await productsModel.insertProduct({ name: "ProdutoX" });
+    const result = await productsModel.getAllProducts();
+    // Assert
+    expect(result).to.be.deep.equal(insertProductMock);
   });
 
   afterEach(function () {
